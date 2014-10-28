8bb9f9818b5aeb54e7d85a4a61936dcdb4687148
Gustavo Henke <gustavo@injoin.io>
2014-10-28 17:52:17 -0200

    chore(update-demo.sh): faz "apenas" push forçado no gh-pages

--- build/update-demo.sh
+++ build/update-demo.sh
@@ -8,8 +8,5 @@ GIT_MAIL=`git show --format="%ae" | head -n 1`
 git config user.name "${GIT_NAME}"
 git config user.email "${GIT_MAIL}"
 
-# Faz merge do master no gh-pages e depois push
-git fetch
-git checkout gh-pages
-git merge master --no-edit
-git push --force --quiet "https://${GH_TOKEN}:x-oauth-basic@github.com/Syonet/scroll.git" gh-pages:gh-pages > /dev/null 2>&1
+# Faz push forçado no gh-pages remoto
+git push --force --quiet "https://${GH_TOKEN}:x-oauth-basic@github.com/Syonet/scroll.git" master:gh-pages > /dev/null 2>&1
